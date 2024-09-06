94a41149ee48f8dd8b35f4988bb2eda6c392114c
Munitor10 <rafamartinsmb10@gmail.com>
2024-09-05 21:44:51 -0300

    Add files via upload

---
+++ AlunoAPIv2/AlunoAPIv2/Repository/PessoaRepository.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace AlunoAPIv2.Repositores
+{
+    public class PessoaRepository
+    {
+        private readonly string _connectionString;
+       
+        public PessoaRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        private IDbConnection Connection => 
+            new MySqlConnection(_connectionString); 
+    }
+}
